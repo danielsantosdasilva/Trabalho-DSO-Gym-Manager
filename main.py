fe77a4027e7a507b906e31076a6879336f565043
danielsantosdasilva <107444265+danielsantosdasilva@users.noreply.github.com>
2022-06-18 17:03:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from Controle.CtrlSistema import CtrlSistema
-from Entidade.professor import Professor
+from banco_dados import BancoDados
 
 if __name__ == "__main__":
     ctrl_sistema = CtrlSistema()
-    professor = Professor("Roberto", "admin123", 35, 12543542354, 80, 1.90, 1111)
-    ctrl_sistema.controlador_professor.professor = professor
+    BancoDados(ctrl_sistema).rodar()
     ctrl_sistema.inicializar()
