898a88c3a15add096f9f445efeab6ba36d732b2e
danielsantosdasilva <107444265+danielsantosdasilva@users.noreply.github.com>
2022-06-18 17:18:00 -0300

    Update modalidade.py

--- Entidade/modalidade.py
+++ Entidade/modalidade.py
@@ -1,14 +1,23 @@
 
 
 class Modalidade:
-    def __init__(self, nome: str, horarios: []):
+    def __init__(self, nome: str, horarios: [], codigo: int):
         self.__nome = nome
         self.__horarios = horarios
+        self.__codigo = codigo
         self.__alunos = []
 
     @property
     def nome(self):
         return self.__nome
+
+    @property
+    def horarios(self):
+        return self.__horarios
+
+    @property
+    def codigo(self):
+        return self.__codigo
 
     @nome.setter
     def nome(self, nome: str):
