c6127dd3016c8f72e6796874cd0186fdd9e11f78
danielsantosdasilva <107444265+danielsantosdasilva@users.noreply.github.com>
2022-06-18 17:07:33 -0300

    Update aluno.py

--- Entidade/aluno.py
+++ Entidade/aluno.py
@@ -4,4 +4,4 @@ from Entidade.pessoa import Pessoa
 class Aluno(Pessoa):
     def __init__(self, nome: str, senha: str, idade: int, peso: float, cpf: int, altura: float, matricula: int):
         super().__init__(nome, senha, idade, peso, cpf, altura, matricula)
-
+        self.__aulas = []
